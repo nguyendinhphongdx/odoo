f4dcfb5398a9094e43cbe7726472281a9f4238d7
nguyendinhphongdx <phongnguyendx@gmail.com>
2021-09-19 09:01:52 +0700

    add icon app

--- src/common/components/ContainerScreen.tsx
+++ src/common/components/ContainerScreen.tsx
@@ -14,19 +14,19 @@ const {width, height} = Dimensions.get('window');
 const heightNBT = Dimensions.get('screen').height;
 const ContainerScreen: React.FC<{color?:string;bottomTab?:boolean,style?:ViewStyle}> = ({children,color,bottomTab,style}) => {
   const backgroundImage = color=='white'?require('../../assets/img/gettingStarted/bg_blue.png'):require('../../assets/img/gettingStarted/bg_blue.png');
-  const heighScreen = bottomTab?height:heightNBT-StatusBar.currentHeight
+  const heighScreen = bottomTab?height:heightNBT
   return (
     <ImageBackground
       source={backgroundImage}
       style={{
         width: width,
-        height:heighScreen,
+        height:heightNBT -StatusBar.currentHeight,
         paddingHorizontal: 10,
         paddingTop: StatusBar.currentHeight,
-        paddingBottom:bottomTab?20:0,
+        paddingBottom:bottomTab?70:0,
         ...style
       }}>
-        <View style={{ backgroundColor:'rgba(255,255,255,.2)',position:'absolute',top:0,left:0,width,height:heighScreen}}/>
+        <View style={{ backgroundColor:'rgba(255,255,255,.2)',position:'absolute',top:0,left:0,width,height:heightNBT}}/>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={{flex:1}}
